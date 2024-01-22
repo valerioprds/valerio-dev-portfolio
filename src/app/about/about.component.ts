81d4f457d85b4a19fd7991e4fbc0187c878d6115
valerioprds <v.paradiso89@gmail.com>
2024-01-22 13:08:11 +0100

    my first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+//import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ContactComponent } from '../contact/contact.component';
+
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+
+ // constructor(private dialogRef: MatDialog) {}
+
+
+ /*  openContactDialog() {
+    this.dialogRef.open(ContactComponent, {
+      width: '900px',
+      height: 'auto',
+    });
+  } */
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, AboutComponent]
 })
 export class AppComponent {
   title = 'valerio-developer';
