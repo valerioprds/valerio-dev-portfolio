74a91c32d4ddc28aeaf7872bb704686add1b89a2
valerioprds <v.paradiso89@gmail.com>
2024-02-05 13:18:14 +0100

    generate tech component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ContactComponent } from '../contact/contact.component';
+import { TechComponent } from "../tech/tech.component";
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [MatDialogModule],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css',
+    selector: 'app-about',
+    standalone: true,
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.css',
+    imports: [MatDialogModule, TechComponent]
 })
 export class AboutComponent {
   constructor(private dialogRef: MatDialog) {}
