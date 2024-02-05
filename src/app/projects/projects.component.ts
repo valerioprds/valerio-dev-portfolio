097157dbf8f4642a8ed9dcfc577257e4596c9e9f
valerioprds <v.paradiso89@gmail.com>
2024-02-05 21:37:59 +0100

    removed browser animation module

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [BrowserAnimationsModule],
+  imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
